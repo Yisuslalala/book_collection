82b86020b70b70967ff76616ee683a670a149800
Jesús Quiñones <1930318@upv.edu.mx>
2023-08-15 00:08:33 -0600

    Some changes aren't working

--- backend/index.js
+++ backend/index.js
@@ -1,81 +1,80 @@
-import express from "express"
-import mysql from "mysql"
-import cors from "cors"
-
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "yisus",
-    password: "1235",
-    database: "book_collection"
+  host: "localhost",
+  user: "yisus",
+  password: "1235",
+  database: "book_collection",
 });
 
 app.get("/", (req, res) => {
-    res.json("Hello");
+  res.json("Hello");
 });
 
 app.get("/books", (req, res) => {
-    const q = "SELECT * FROM books";
-    db.query(q, (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    });
+  const q = "SELECT * FROM books";
+  db.query(q, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
 app.post("/books", (req, res) => {
-    const title = req.body.title;
-    const description = req.body.description;
-    const cover = req.body.cover;
-    const price = req.body.price;
-    // console.log(title, description, cover);
-    const q = "INSERT INTO books (title, description, cover, price) VALUES(?, ?, ?, ?)"
-    db.query(q, [title, description, cover, price], (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    });
+  const title = req.body.title;
+  const description = req.body.description;
+  const cover = req.body.cover;
+  const price = req.body.price;
+  // console.log(title, description, cover);
+  const q =
+    "INSERT INTO books (title, description, cover, price) VALUES(?, ?, ?, ?)";
+  db.query(q, [title, description, cover, price], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
-app.delete("/books/:id", (req, res) => {
-    const bookId = req.params.book_id;
-    // console.log(bookId);
-    const q = "DELETE FROM books WHERE book_id = ?";
-    db.query(q, [bookId], (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send("Book has been deleted");
-        }
-    });
+app.delete("/delete/:id", (req, res) => {
+  const bookId = req.params.book_id;
+  console.log(bookId);
+  db.query("DELETE FROM books WHERE book_id = ?", bookId, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
 app.put("/books/:id", (req, res) => {
-    const bookId = req.params.book_id;
-    const q = "UPDATE books SET title = ?, description = ?, price = ?, cover = ? WHERE book_id = ?";
-    const title = req.body.title;
-    const description = req.body.description;
-    const cover = req.body.cover;
-    const price = req.body.price;
-    
-    db.query(q, [title, description, cover, price, bookId], (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send("Book updated successfully");
-        }
-    });
+  const bookId = req.params.book_id;
+  const q =
+    "UPDATE books SET title = ?, description = ?, price = ?, cover = ? WHERE book_id = ?";
+  const title = req.body.title;
+  const description = req.body.description;
+  const cover = req.body.cover;
+  const price = req.body.price;
+
+  db.query(q, [title, description, cover, price, bookId], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send("Book updated successfully");
+    }
+  });
 });
 
 app.listen(8000, () => {
-    console.log("Connected to backend in port 8000")
+  console.log("Connected to backend in port 8000");
 });
-
